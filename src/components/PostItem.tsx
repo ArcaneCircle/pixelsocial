73bf4e84bdadaa3d3514fa3149f0aeb46aca48d4
adbenitez <asieldbenitez@gmail.com>
2025-07-05 04:13:36 +0200

    add "show more" to long posts

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -1,17 +1,25 @@
-import { useRef, useCallback, useContext } from "react";
+import { useState, useRef, useCallback, useContext } from "react";
 import { toJpeg } from "html-to-image";
 
+import { TEXT_TRUNCATE_SIZE, TEXT_TRUNCATE_LINES } from "~/constants";
 import { ManagerContext } from "~/contexts";
+import { _ } from "~/lib/i18n";
 
 import BasePostItem from "~/components/BasePostItem";
 import PostActionsBar from "~/components/PostActionsBar";
 import PostImage from "~/components/PostImage";
+
+const linkStyle = {
+  color: "#ccae3a",
+  cursor: "pointer",
+};
 
 interface Props {
   post: Post;
 }
 
 export default function PostItem({ post }: Props) {
+  const [showMore, setShowMore] = useState(false);
   const manager = useContext(ManagerContext);
   const textRef = useRef<HTMLDivElement | null>(null);
 
@@ -33,7 +41,28 @@ export default function PostItem({ post }: Props) {
     window.webxdc.sendToChat({ file, text });
   }, [post.text, post.image, post.style, textRef]);
 
+  const onShowMore = useCallback(() => {
+    setShowMore(true);
+  }, [post.id]);
+
   const deletePost = useCallback(() => manager.deletePost(post.id), [post.id]);
+
+  let truncated = false;
+  let text = post.text;
+  if (!post.style && !showMore) {
+    if (text.length > TEXT_TRUNCATE_SIZE) {
+      truncated = true;
+      text = text.substr(0, TEXT_TRUNCATE_SIZE);
+    }
+    const lines = text.split("\n");
+    if (lines.length > TEXT_TRUNCATE_LINES) {
+      truncated = true;
+      text = lines.slice(0, TEXT_TRUNCATE_LINES).join("\n");
+    }
+    if (truncated) {
+      text += "... ";
+    }
+  }
 
   return (
     <BasePostItem
@@ -47,7 +76,12 @@ export default function PostItem({ post }: Props) {
         ref={textRef}
         className={post.style ? `card grad${post.style}` : "hpad08"}
       >
-        {post.text}
+        {text}
+        {truncated && (
+          <span style={linkStyle} onClick={onShowMore}>
+            {_("Show More...")}
+          </span>
+        )}
       </div>
       {post.image && <PostImage src={post.image} />}
       <PostActionsBar className="hpad08" post={post} onShare={onShare} />
--- src/components/ReplyItem.tsx
+++ src/components/ReplyItem.tsx
@@ -1,19 +1,47 @@
-import { useCallback, useContext } from "react";
+import { useState, useCallback, useContext } from "react";
 
+import { TEXT_TRUNCATE_SIZE, TEXT_TRUNCATE_LINES } from "~/constants";
 import { ManagerContext } from "~/contexts";
+import { _ } from "~/lib/i18n";
 
 import BasePostItem from "~/components/BasePostItem";
+
+const linkStyle = {
+  color: "#ccae3a",
+  cursor: "pointer",
+};
 
 interface Props {
   reply: Reply;
 }
 
 export default function ReplyItem({ reply }: Props) {
+  const [showMore, setShowMore] = useState(false);
   const manager = useContext(ManagerContext);
   const deleteReply = useCallback(
     () => manager.deleteReply(reply.postId, reply.id),
     [reply.id],
   );
+  const onShowMore = useCallback(() => {
+    setShowMore(true);
+  }, [reply.id]);
+
+  let truncated = false;
+  let text = reply.text;
+  if (!showMore) {
+    if (text.length > TEXT_TRUNCATE_SIZE) {
+      truncated = true;
+      text = text.substr(0, TEXT_TRUNCATE_SIZE);
+    }
+    const lines = text.split("\n");
+    if (lines.length > TEXT_TRUNCATE_LINES) {
+      truncated = true;
+      text = lines.slice(0, TEXT_TRUNCATE_LINES).join("\n");
+    }
+    if (truncated) {
+      text += "... ";
+    }
+  }
 
   return (
     <BasePostItem
@@ -24,7 +52,14 @@ export default function ReplyItem({ reply }: Props) {
       deletePost={deleteReply}
       style={{ backgroundColor: "#23232f" }}
     >
-      <span className="hpad08">{reply.text}</span>
+      <div className="hpad08">
+        {text}
+        {truncated && (
+          <span style={linkStyle} onClick={onShowMore}>
+            {_("Show More...")}
+          </span>
+        )}
+      </div>
     </BasePostItem>
   );
 }
