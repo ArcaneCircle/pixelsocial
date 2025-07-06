a873833af43b642128ac626081084c6db3333eaf
adbenitez <asieldbenitez@gmail.com>
2025-07-06 21:08:33 +0200

    show admin badge

--- src/components/UserItem.tsx
+++ src/components/UserItem.tsx
@@ -1,4 +1,5 @@
-import { GRAY_COLOR } from "~/constants";
+import { GRAY_COLOR, ACCENT_COLOR } from "~/constants";
+import { _ } from "~/lib/i18n";
 
 import MonsterAvatar from "~/components/MonsterAvatar";
 
@@ -13,6 +14,11 @@ const rightStyle = {
   flexDirection: "column" as "column",
   overflow: "hidden",
 };
+const flexRow = {
+  display: "flex",
+  flexDirection: "row" as "row",
+  gap: "0.2em",
+};
 const nameStyle = {
   overflow: "hidden",
   textOverflow: "ellipsis",
@@ -23,6 +29,12 @@ const subtitleStyle = {
   color: GRAY_COLOR,
   textWrap: "nowrap" as "nowrap",
   fontSize: "0.8em",
+};
+
+const badgeStyle = {
+  border: "1px solid " + ACCENT_COLOR,
+  color: ACCENT_COLOR,
+  padding: "0 0.2em",
 };
 
 interface Props {
@@ -42,18 +54,17 @@ export default function UserItem({
 }: Props) {
   return (
     <div style={containerStyle} {...props}>
-      {isAdmin ? (
-        <img width="40px" height="40px" src="./icon.png" />
-      ) : (
-        <MonsterAvatar
-          value={userId}
-          width={40}
-          height={40}
-          style={{ background: "black" }}
-        />
-      )}
+      <MonsterAvatar
+        value={userId}
+        width={40}
+        height={40}
+        style={{ background: "black" }}
+      />
       <div style={rightStyle}>
-        <span style={nameStyle}>{name}</span>
+        <div style={flexRow}>
+          <span style={nameStyle}>{name}</span>
+          {isAdmin && <span style={badgeStyle}>{_("admin")}</span>}
+        </div>
         {subtitle && <span style={subtitleStyle}>{subtitle}</span>}
       </div>
     </div>
